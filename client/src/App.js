ea857a904623b3e7aab83de1ddd41b42018cc75e
strisen <signum.aeterna@gmail.com>
2020-06-18 17:04:35 +0800

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator Version 2
+            Fib Calculator Version KUBERNETES!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
